c7e4b6955ee113d04dfc7fa33c09416801f21dea
James Addison <55152140+jayaddison@users.noreply.github.com>
2009-08-02 00:01:56 +0100

    Logstore build migration

--- dbs/logstore/create.sql
+++ dbs/logstore/create.sql
@@ -1,3 +1,5 @@
+create language plpgsql;
+
 create table tbaccesslog
 (
     id serial,
--- dbs/logstore/functions/fn_logsearch.sql
+++ dbs/logstore/functions/fn_logsearch.sql
@@ -2,9 +2,11 @@ create or replace function fn_logsearch
 (
     log_searchtext text
 )
-returns void as $$
+returns void as
+'
 begin
     insert into tbsearches (searchtext)
     select log_searchtext;
 end
-$$ language plpgsql;
+'
+language plpgsql;
