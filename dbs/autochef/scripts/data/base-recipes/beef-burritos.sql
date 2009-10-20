b7b1744ad9f407ba2cba5a7df36cb39af9b62782
James Addison <55152140+jayaddison@users.noreply.github.com>
2009-10-20 23:10:11 +0100

    Database: temporary-ish db build fixes

--- dbs/autochef/scripts/data/base-recipes/beef-burritos.sql
+++ dbs/autochef/scripts/data/base-recipes/beef-burritos.sql
@@ -4,13 +4,6 @@ insert into tbmeals (ingredientid) values (currval('tbingredients_ingredientid_s
 insert into tbingredienttranslations (ingredientid, languageid, name, instructions) values (currval('tbingredients_ingredientid_seq'), 'en', 'Beef Burritos', '');
 
 -- Required Ingredients
-insert into tbingredients (duration) values ('0'::interval);
-insert into tbproducts (ingredientid, supplierid) values (currval('tbingredients_ingredientid_seq'), '');
-insert into tbingredienttranslations (ingredientid, languageid, name, instructions) values (currval('tbingredients_ingredientid_seq'), 'en', 'Beef', '');
-
-insert into tbingredients (duration) values ('5 minutes'::interval);
-insert into tbproducts (ingredientid, supplierid) values (currval('tbingredients_ingredientid_seq'), '');
-insert into tbingredienttranslations (ingredientid, languageid, name, instructions) values (currval('tbingredients_ingredientid_seq'), 'en', 'Minced Beef', '');
 
 -- Intermediate Steps
 
--- dbs/autochef/scripts/data/base-recipes/beef-stew.sql
+++ dbs/autochef/scripts/data/base-recipes/beef-stew.sql
@@ -4,9 +4,6 @@ insert into tbmeals (ingredientid) values (currval('tbingredients_ingredientid_s
 insert into tbingredienttranslations (ingredientid, languageid, name, instructions) values (currval('tbingredients_ingredientid_seq'), 'en', 'Beef Stew', '');
 
 -- Required Ingredients
-insert into tbingredients (duration) values ('0'::interval);
-insert into tbproducts (ingredientid, supplierid) values (currval('tbingredients_ingredientid_seq'), '');
-insert into tbingredienttranslations (ingredientid, languageid, name, instructions) values (currval('tbingredients_ingredientid_seq'), 'en', 'Beef', '');
 
 -- Intermediate Steps
 
